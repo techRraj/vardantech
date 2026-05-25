21d5753c7b9c330466839622b4006f1a71d1e20a
rajkumar chourasiya <rajchourasiya2862002@gmail.com>
2026-05-25 17:05:56 +0530

    commit for frontend

--- src/components/UI/Loader.jsx
+++ src/components/UI/Loader.jsx
@@ -10,7 +10,8 @@ const Loader = () => (
   >
     <motion.img
       // src="/assets/logo.png"
-      src='/src/assets/logo3.png'
+      // src='/src/assets/logo3.png'
+      src='../../../public/assets/logo3.png'
       alt="VardaanTechHub"
       className={styles.logo}
       animate={{ scale: [1, 1.05, 1], rotate: [0, 5, -5, 0] }}
